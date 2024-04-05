41474b8dba508dbebd97c8f0c0915be462d2167c
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-04-05 15:51:01 +0530

    added model class

---
+++ lib/model/model db.dart
@@ -0,0 +1,14 @@
+class StudentModel {
+  int? id;
+  final String name;
+  final String classRoom;
+  final String guardian;
+  final String phone;
+  StudentModel({
+    this.id,
+    required this.name,
+    required this.classRoom,
+    required this.guardian,
+    required this.phone,
+  });
+}
